f26a375cc639f8faa2cb0247dbfa896e6ed9f9c4
ListratenkoIgor <listrik_individum@mail.ru>
2020-12-21 04:22:28 +0300

    add resourses

--- Chess/WindowsChess/Resource.h
+++ Chess/WindowsChess/Resource.h
@@ -3,8 +3,8 @@
 // Используется WindowsChess.rc
 //
 #define IDC_MYICON                      2
-#define BLACK_R                         2
 #define IDD_WINDOWSCHESS_DIALOG         102
+#define IDD_CHOOSE_DIALOG               102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -13,16 +13,40 @@
 #define IDI_SMALL                       108
 #define IDC_WINDOWSCHESS                109
 #define IDR_MAINFRAME                   128
-#define IDB_BITMAP1                     132
-#define IDC_STATIC                      -1
+#define ID_LEVEL_EASY                   32776
+#define ID_LEVEL_MEDIUM                 32777
+#define ID_LEVEL_HARD                   32778
+#define ID_NEWGAME                      32779
+#define ID_ENEMY_HUMAN					32780
+#define ID_ENEMY_COMPUTER				32781
+#define ID_COLOR_WHITE					32782
+#define ID_COLOR_BLACK					32783
+
+#define IDT_GAME_TIMERID                31001
+#define IDC_LISTBOX_WHITE               31002
+#define IDC_LISTBOX_BLACK               32003
+#define IDC_STATIC						-1
+#define BLACK_P							11
+#define BLACK_R                         12
+#define BLACK_N                         13
+#define BLACK_B                         14
+#define BLACK_Q                         15
+#define BLACK_K                         16
+#define WHITE_P                         17
+#define WHITE_R                         18
+#define WHITE_N                         19
+#define WHITE_B                         20
+#define WHITE_Q                         21
+#define WHITE_K                         22
+
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32780
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
